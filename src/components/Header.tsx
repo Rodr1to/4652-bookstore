29f44e87dc6a26de7df8e068cd60bf37c65a5301
rodr1to <rovalverdea@gmail.com>
2025-10-11 12:55:41 -0500

    fixed minor errors in Header and netlify.toml

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,7 +20,6 @@ const Header: React.FC = () => {
             </span>
           </NavLink>
 
-          {/* Contenedor corregido para agrupar los elementos de la derecha */}
           <div className="flex items-center gap-8">
             <nav className="hidden md:flex space-x-8 items-center">
               <NavLink to="/" style={({ isActive }) => isActive ? activeLinkStyle : undefined} className="text-gray-600 hover:text-brand-green transition-colors duration-300 font-semibold">
@@ -33,7 +32,8 @@ const Header: React.FC = () => {
                 Catálogo Completo
               </NavLink>
               <NavLink to="/carrito" className="relative text-gray-600 hover:text-brand-green transition-colors">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" fill="none" viewBox="0.0 24 24" stroke="currentColor">
+                {/* LÍNEA CORREGIDA */}
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
                 </svg>
                 {totalItems > 0 && (
